7c7301c115d81319ac0753b8d407a9b87a44e71a
Alex Tercete <alex.tercete@red-gate.com>
2019-09-06 14:56:03 +0100

    Replace run scripts with cross-platform Makefile

--- _run.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-rem In case eventmachine fails to load:
-rem gem uninstall eventmachine
-rem gem install eventmachine --platform=ruby
-set PAGES_REPO_NWO=flyway/flywaydb.org
-bundle exec jekyll serve -w -L
--- _run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker run --rm \
-  -v "$(PWD):/srv/jekyll" \
-  -p 35729:35729 -p 4000:4000 \
-  -e PAGES_REPO_NWO='flyway/flywaydb.org' \
-  -it jekyll/jekyll:3.8 \
-  jekyll serve --livereload --incremental
